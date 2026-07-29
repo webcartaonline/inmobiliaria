119357d41e4d2826b17431b88d2c77b2c9e29d1a
Marisa-Ruiz <marisa120685@gmail.com>
2026-07-30 00:04:12 +0200

    fix: remove reservation button from Header (belongs to Nav)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,6 @@ function Header() {
   return (
     <header className="header">
       <div className="headerLogo">Nido Estudiantil Granada</div>
-      <button className="headerCta">Reservar visita</button>
     </header>
   );
 }
