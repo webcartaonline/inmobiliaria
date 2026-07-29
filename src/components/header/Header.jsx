32ecce3cc8db4262bde358cab08e71201c58fad1
Marisa-Ruiz <marisa120685@gmail.com>
2026-07-29 16:20:17 +0200

    feat: add Header layout and structural markup

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="headerLogo">Nido Estudiantil Granada</div>
+      <button className="headerCta">Reservar visita</button>
+    </header>
+  );
+}
+
+export default Header;
