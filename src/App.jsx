5fc9c7f3a67041aa22e5834c5cd57c4ccf675000
webcartaonline <webcartaonline@gmail.com>
2026-07-30 13:03:55 +0200

    feature: card/flats

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import Footer from './components/Footer/Footer';
+import PisoCard from './components/cardFlats';
+import images from './utils/images';
 
 function App() {
     return (
-        <div className="appContainer">
-            <main>
-
-            </main>
-
-
-            <Footer />
-        </div>
+        <main>
+            <PisoCard
+                image={images['Window.png']}
+                price="450€/MES"
+                title="Piso en el Realejo"
+                distance="8 min a la UGR"
+                services={['WiFi 1GB', 'Climatizado']}
+            />
+            <PisoCard
+                image={images['Groound.png']}
+                price="50€/MES"
+                title="Piso felíz navidad"
+                distance="10 min a la UGR"
+                services={['WiFi 1GB', 'Climatizado', 'Piscina']}
+            />
+        </main>
     );
 }
+
 export default App;
-ad
---
+++ src/components/cardFlats.jsx
@@ -0,0 +1,30 @@
+import '../styles/cardFlats.css';
+
+function PisoCard({ image, price, title, distance, services }) {
+    return (
+        <article className="card">
+            <div className="imageWrapper">
+                <img className="image" src={image} alt={title} />
+                <span className="price">{price}</span>
+            </div>
+
+            <div className="info">
+                <h3 className="title">{title}</h3>
+
+                <p className="distance">
+                    {distance}
+                </p>
+
+                <ul className="services">
+                    {services.map((service) => (
+                        <li key={service} className="service">
+                            {service}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </article>
+    );
+}
+
+export default PisoCard;
