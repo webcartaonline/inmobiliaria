3b51d9e4f64f6bbf33b6395a90ca480cf8e0dada
Marisa-Ruiz <marisa120685@gmail.com>
2026-07-30 12:56:49 +0200

    feat: integrate Hero into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from './components/header/Header';
+import Hero from './components/hero/Hero';
 
 function App() {
   return (
+    <>
       <Header />
+      <Hero />
+    </>
   );
 }
 
