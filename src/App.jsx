a1c5b6ea21777a065e5c34d163ff36e5574a265f
webcartaonline <webcartaonline@gmail.com>
2026-07-30 09:46:29 +0200

    feature: conexión files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from './components/Footer/Footer';
+
+function App() {
+    return (
+        <div className="appContainer">
+            <main>
+
+            </main>
+
+
+            <Footer />
+        </div>
+    );
+}
+export default App;
+ad
