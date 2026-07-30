48efb3ccb49380f7bab914c92ccca6a47edbcd00
webcartaonline <webcartaonline@gmail.com>
2026-07-30 13:57:04 +0200

    fix: cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import images from './utils/images';
 function App() {
     return (
         <main>
-            <PisoCard
-                image={images['Window.png']}
-                price="450€/MES"
-                title="Piso en el Realejo"
-                distance="8 min a la UGR"
-                services={['WiFi 1GB', 'Climatizado']}
-            />
-            <PisoCard
-                image={images['Groound.png']}
-                price="50€/MES"
-                title="Piso felíz navidad"
-                distance="10 min a la UGR"
-                services={['WiFi 1GB', 'Climatizado', 'Piscina']}
-            />
+            <div className="cardsContainer">
+                <PisoCard
+                    image={images['Window.png']}
+                    price="450€/MES"
+                    title="Piso en el Realejo"
+                    distance="8 min a la UGR"
+                    services={['WiFi 1GB', 'Climatizado']}
+                />
+                <PisoCard
+                    image={images['Groound.png']}
+                    price="500€/MES"
+                    title="Piso felíz navidad"
+                    distance="10 min a la UGR"
+                    services={['WiFi 1GB', 'Climatizado', 'Piscina']}
+                />
+            </div>
         </main>
     );
 }
