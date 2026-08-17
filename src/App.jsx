fb55373de5c4851d2f367bd0b80aafa41973d4ce
oscarmmejia <neo_9615@hotmail.com>
2026-08-17 13:17:21 +0200

    hide bar search

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
       <main>
         <Hero />
-        <PropertySearchBar />
+        {/* <PropertySearchBar /> */}
         <About />
         <AgentsSection />
       </main>
