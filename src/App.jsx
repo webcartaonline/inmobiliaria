8edfa36fbddf5b2c51ca9b641eb0c84608defc96
carlosjper <cjavierp500@gmail.com>
2026-08-18 19:34:24 +0200

    feat: setup react router dom in main and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
+
 import "./styles/App.css";
-import { PropertySearchBar } from "./components/search/organisms/PropertySearchBar";
-import About from "./components/About/About";
-import { AgentsSection } from "./components/agents/organisms/AgentsSection";
-import Footer from "./components/Footer/Footer";
+import { Routes, Route } from "react-router-dom"; // <-- Importamos los componentes de rutas
+
+// Componentes fijos (comunes a todas las páginas)
 import Header from "./components/header/Header";
-import Hero from "./components/hero/Hero";
-import FlatsSection from "./components/FlatsSection/FlatsSection";
+import Footer from "./components/footer/Footer";
+
+// Vistas completas desde la carpeta src/pages/
+import Home from "./pages/Home";
+import Agents from "./pages/Agents";
+import History from "./pages/History";
+import Flats from "./pages/Flats";
+import Restaurant from "./pages/Restaurant";
 
 function App() {
   return (
     <div className="appContainer">
+      {/* El Header estará visible en TODAS las páginas */}
       <Header />
 
+      {/* Dentro de <main> cambia la habitación según la URL */}
       <main>
-        <Hero />
-        {/* <PropertySearchBar /> */}
-        <About />
-        <FlatsSection />
-        <AgentsSection />
+        <Routes>
+          {/* Ruta principal (Inicio): aquí vive la Landing actual con Hero, About, etc. */}
+          <Route path="/" element={<Home />} />
+          <Route path="/agents" element={<Agents />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/flats" element={<Flats />} />
+          <Route path="/restaurant" element={<Restaurant />} />
+        </Routes>
       </main>
 
+      {/* El Footer estará visible en TODAS las páginas */}
       <Footer />
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // <-- 1. Importamos BrowserRouter
 import './styles/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* <-- 2. Envolvemos App aquí */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
