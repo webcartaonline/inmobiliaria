0b33b700298a8e388fccff6a52623fa6c4cb33ed
Marisa-Ruiz <marisa120685@gmail.com>
2026-08-19 19:42:16 +0200

    feat: build Home page composing Hero, About, FlatsSection and AgentsSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
+import Hero from '../components/hero/Hero';
+import About from '../components/about/About';
+import FlatsSection from '../components/flatsSection/FlatsSection';
+import { AgentsSection } from '../components/agents/organisms/AgentsSection';
+
 function Home() {
   return (
-    <main>
-      <h1>Inicio</h1>
-      <p>Página de inicio en construcción.</p>
-    </main>
+    <>
+      <Hero />
+      <About />
+      <FlatsSection />
+      <AgentsSection />
+    </>
   );
 }
 
